cdc0f16f72eb9f006c4e502825192b814c0109b3
Atharv Dalal <sangamdalal24@gmail.com>
2025-02-03 21:34:44 +0530

    jwt startegy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+// jwt.strategy.ts
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.SECRETE_KEY,
+    });
+  }
+
+  async validate(payload: any) {
+    return this.usersRepository.findOne(payload.userId);
+  }
+}
