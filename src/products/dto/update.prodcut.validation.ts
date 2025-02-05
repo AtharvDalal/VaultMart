821e08e06951a881de19f41fea866cb7fd3c94c9
Atharv Dalal <sangamdalal24@gmail.com>
2025-02-05 17:38:14 +0530

    schema validation using joi

---
+++ src/products/dto/update.prodcut.validation.ts
@@ -0,0 +1,27 @@
+import * as Joi from 'joi';
+import { Category } from 'src/common/enums/category';
+
+export const updateProduct = Joi.object({
+  productname: Joi.string().min(2).max(100).optional().messages({
+    'string.min': 'Min 2 characters required for product name',
+    'string.max': 'Max 100 characters allowed for product name',
+  }),
+
+  desc: Joi.string().min(2).max(50).optional().messages({
+    'string.min': 'Min 2 characters required for description',
+    'string.max': 'Max 50 characters allowed for description',
+  }),
+
+  price: Joi.number().min(1).positive().optional().messages({
+    'number.min': 'Price must be at least 1',
+    'number.positive': 'Price must be a positive number',
+  }),
+
+  category: Joi.string()
+    .valid(Category.CLOTHING, Category.ELECTRONICS, Category.FURNITURE)
+    .optional()
+    .messages({
+      'any.only':
+        'Category must be one of the following: clothing, electronics, or furniture',
+    }),
+});
